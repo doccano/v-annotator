a434271991e0f48ec0c616d2bcf2c66cf65f89ea
Hironsan <light.tree.1.13@gmail.com>
2021-06-22 14:55:45 +0900

    Add VAnnotator component

---
+++ src/components/VAnnotator.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    entities: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+    relations: {
+      type: Array,
+      default: () => [],
+      required: false,
+    },
+    entityLabels: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+    relationLabels: {
+      type: Array,
+      default: () => [],
+      required: false,
+    },
+    allowOverlapping: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    allowMultipleRelation: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+});
+</script>
