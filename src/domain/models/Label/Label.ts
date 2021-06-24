c1e965e96c01137a138e4e063d7e076e3edd49d7
Hironsan <light.tree.1.13@gmail.com>
2021-06-24 14:52:20 +0900

    Add valueOf to Labels

--- src/domain/models/Label/Label.ts
+++ src/domain/models/Label/Label.ts
@@ -7,3 +7,34 @@ export class Label {
     public color: string
   ) {}
 }
+
+interface ILabel {
+  id: number;
+  text: string;
+  prefixKey: string | null;
+  suffixKey: string | null;
+  color: string;
+}
+
+export class Labels {
+  constructor(private labels: Label[]) {}
+
+  static valueOf(labels: ILabel[]): Labels {
+    return new Labels(
+      labels.map(
+        (label) =>
+          new Label(
+            label.id,
+            label.text,
+            label.prefixKey,
+            label.suffixKey,
+            label.color
+          )
+      )
+    );
+  }
+
+  getById(id: number): Label | undefined {
+    return this.labels.find((label) => label.id === id);
+  }
+}
