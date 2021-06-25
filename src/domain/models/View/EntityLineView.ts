5a478570e0faf956335007cdf96e7c4375592cfd
Hironsan <light.tree.1.13@gmail.com>
2021-06-25 15:25:17 +0900

    Update EntityLineView.ts

--- src/domain/models/View/EntityLineView.ts
+++ src/domain/models/View/EntityLineView.ts
@@ -23,11 +23,11 @@ export class EntityLineView {
         "g"
       ) as SVGGElement;
       const lineElement = this.createLineElement(entity, x1, x2);
-      const circleElement = this.createCircleElement(entity, x1);
-      const textElement = this.createLabelTextElement(entity, x1);
       entityLabelElement.appendChild(lineElement);
       // Do not show a label text if the entity continues from the previous line.
       if (this.textLine.startOffset <= entity.startOffset) {
+        const circleElement = this.createCircleElement(entity, x1);
+        const textElement = this.createLabelTextElement(entity, x1);
         entityLabelElement.appendChild(circleElement);
         entityLabelElement.appendChild(textElement);
       }
@@ -47,15 +47,29 @@ export class EntityLineView {
     return label!.text;
   }
 
+  private calculateLineY(entity: Entity): number {
+    const level = this.entities.getLevelOf(entity.id)!;
+    return 6 * (level + 1);
+  }
+
+  private calculateTextY(entity: Entity): number {
+    const level = this.entities.getLevelOf(entity.id)!;
+    const overlap = this.entities.getOverlapOf(entity.id)!;
+    const width = 3;
+    const lineHeight = width * (2 * overlap - 1);
+    return 20 * (level + 1) + lineHeight;
+  }
+
   private createLineElement(entity: Entity, x1: number, x2: number) {
     const lineElement = document.createElementNS(
       SVGNS,
       "line"
     ) as SVGLineElement;
+    const y = this.calculateLineY(entity);
     lineElement.setAttribute("x1", x1.toString());
     lineElement.setAttribute("x2", x2.toString());
-    lineElement.setAttribute("y1", "6");
-    lineElement.setAttribute("y2", "6");
+    lineElement.setAttribute("y1", y.toString());
+    lineElement.setAttribute("y2", y.toString());
     lineElement.setAttribute("stroke-width", "3");
     lineElement.setAttribute("stroke-linecap", "round");
     lineElement.setAttribute("stroke", this.color(entity));
@@ -67,10 +81,12 @@ export class EntityLineView {
       SVGNS,
       "circle"
     ) as SVGCircleElement;
+    const r = 3;
+    const y = this.calculateTextY(entity);
     circleElement.setAttribute("fill", this.color(entity));
-    circleElement.setAttribute("cx", x1.toString());
-    circleElement.setAttribute("r", "3");
-    circleElement.setAttribute("cy", "20");
+    circleElement.setAttribute("cx", (x1 + r / 2).toString());
+    circleElement.setAttribute("r", r.toString());
+    circleElement.setAttribute("cy", y.toString());
     return circleElement;
   }
 
@@ -79,9 +95,10 @@ export class EntityLineView {
       SVGNS,
       "text"
     ) as SVGTextElement;
+    const y = this.calculateTextY(entity);
     textElement.setAttribute("fill", "dimgrey");
     textElement.setAttribute("x", (x1 + 7).toString());
-    textElement.setAttribute("y", "20");
+    textElement.setAttribute("y", y.toString());
     textElement.setAttribute("dy", "0.35em");
     textElement.textContent = this.labelText(entity);
     return textElement;
