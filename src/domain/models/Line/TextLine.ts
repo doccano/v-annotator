4aa04d8d925f2fdd8895d784a77ad3853a46c7fb
Hironsan <light.tree.1.13@gmail.com>
2021-06-25 13:45:27 +0900

    Simplify TextLine.ts

--- src/domain/models/Line/TextLine.ts
+++ src/domain/models/Line/TextLine.ts
@@ -1,33 +1,21 @@
 export class TextLine {
   constructor(
-    private _content: string,
-    private _startOffset: number,
-    private _endOffset: number,
+    readonly content: string,
+    readonly startOffset: number,
+    readonly endOffset: number,
     private vocab: Map<string, number>
   ) {}
 
-  get content(): string {
-    return this._content;
-  }
-
-  get startOffset(): number {
-    return this._startOffset;
-  }
-
-  get endOffset(): number {
-    return this._endOffset;
-  }
-
   range(startOffset: number, endOffset: number): [number, number] {
     const x1 = Array.from(
-      this._content.substring(0, startOffset - this.startOffset)
+      this.content.substring(0, startOffset - this.startOffset)
     )
       .map((ch) => this.vocab.get(ch)!)
       .reduce((p, x) => p + x, 0);
     const x2 =
       x1 +
       Array.from(
-        this._content.substring(
+        this.content.substring(
           startOffset - this.startOffset,
           endOffset - this.startOffset
         )
