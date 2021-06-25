4ebdf97bf4a1efe259dbd4befa0a9b377e8a853c
Hironsan <light.tree.1.13@gmail.com>
2021-06-25 13:16:17 +0900

    Update TextLineView

--- src/domain/models/View/TextLineView.ts
+++ src/domain/models/View/TextLineView.ts
@@ -2,27 +2,18 @@ import { TextLine } from "../Line/TextLine";
 import { SVGNS } from "../Character/SVGNS";
 
 export class TextLineView {
-  private _y: number;
+  constructor(
+    private textLine: TextLine,
+    private textElement: SVGTextElement
+  ) {}
 
-  constructor(private textLine: TextLine, private textElement: SVGTextElement) {
-    this.textLine = textLine;
-    this.textElement = textElement;
-    this._y = 0;
-  }
-
-  get height(): number {
+  render(): SVGTSpanElement {
     const textSpanElement = document.createElementNS(
       SVGNS,
       "tspan"
     ) as SVGTSpanElement;
-    this.textElement.appendChild(textSpanElement);
     textSpanElement.textContent = this.textLine.content;
-    const height = textSpanElement.getBBox().height;
-    this.textElement.removeChild(textSpanElement);
-    return height;
-  }
-
-  set y(y: number) {
-    this._y = y;
+    this.textElement.appendChild(textSpanElement);
+    return textSpanElement;
   }
 }
