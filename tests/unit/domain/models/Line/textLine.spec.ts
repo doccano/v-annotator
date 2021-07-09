dca9edaac460230d902f4846442888aab70c19c7
Hironsan <light.tree.1.13@gmail.com>
2021-07-09 09:08:22 +0900

    Delete textLine.spec.ts

--- tests/unit/domain/models/Line/textLine.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TextLine } from "@/domain/models/Line/TextLine";
-
-describe("Line component", () => {
-  it("can get its content", () => {
-    const content = "example text";
-    const line = new TextLine(content, 0, content.length, new Map());
-    expect(line.content).toMatch(content);
-  });
-});
