92652d5f18797fd4b8fc532564199239a23c9e33
Hironsan <light.tree.1.13@gmail.com>
2021-07-09 13:06:12 +0900

    Update Font.ts

--- src/domain/models/Line/Font.ts
+++ src/domain/models/Line/Font.ts
@@ -7,13 +7,9 @@ export class Font {
     readonly width: Map<string, number>
   ) {}
 
-  widthOf(text: Array<string> | string): number {
-    if (typeof text === "string") {
-      return this.widthOf(Array.from(text));
-    } else {
-      return text
-        .map((ch) => this.width.get(ch)!)
-        .reduce((a: number, b: number) => a + b, 0);
-    }
+  widthOf(text: string): number {
+    return Array.from(text)
+      .map((ch) => this.width.get(ch)!)
+      .reduce((a: number, b: number) => a + b, 0);
   }
 }
