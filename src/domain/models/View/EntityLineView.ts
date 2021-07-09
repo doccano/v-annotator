1c17a8b1966907a25aa5310358b136d27cfcdf5a
Hironsan <light.tree.1.13@gmail.com>
2021-07-09 15:52:38 +0900

    Update EntityLineView.ts

--- src/domain/models/View/EntityLineView.ts
+++ src/domain/models/View/EntityLineView.ts
@@ -25,40 +25,48 @@ export class EntityLineView {
   render(content: string): SVGGElement {
     const elements = document.createElementNS(SVGNS, "g") as SVGGElement;
     for (const entity of this.entities.list()) {
-      const entityLabel = this.entityLabels.getById(entity.label);
       const [x1, x2] = this.textLine.range(
         content,
         entity.startOffset,
         entity.endOffset
       );
-      const entityLabelElement = document.createElementNS(
-        SVGNS,
-        "g"
-      ) as SVGGElement;
-      const line = new GeometricLine(
-        x1,
-        x2,
-        this.calculateLineY(entity),
-        lineWidth,
-        entityLabel!.color
-      );
-      entityLabelElement.appendChild(line.render());
-      // Do not show a label text if the entity continues from the previous line.
-      if (
-        this.showLabelText &&
-        this.textLine.startOffset <= entity.startOffset
-      ) {
-        const marginTop = 5;
-        const y = this.calculateLineY(entity) + fontHeight / 2 + marginTop;
-        const circle = new GeometricCircle(x1, y, entityLabel!.circle);
-        const text = new GeometricText(x1, y, entityLabel!);
-        entityLabelElement.appendChild(circle.render());
-        entityLabelElement.appendChild(text.render());
+      const element = document.createElementNS(SVGNS, "g") as SVGGElement;
+      this.addLine(element, x1, x2, entity);
+      if (this.canDisplayText(entity)) {
+        this.addText(element, x1, entity);
       }
-      elements.appendChild(entityLabelElement);
+      elements.appendChild(element);
     }
     this.svgElement.appendChild(elements);
     return elements;
+  }
+
+  private canDisplayText(entity: Entity) {
+    // Do not show a label text if the entity continues from the previous line.
+    const startsWithLine = this.textLine.startOffset <= entity.startOffset;
+    return this.showLabelText && startsWithLine;
+  }
+
+  private addLine(
+    element: SVGGElement,
+    x1: number,
+    x2: number,
+    entity: Entity
+  ) {
+    const entityLabel = this.entityLabels.getById(entity.label);
+    const y = this.calculateLineY(entity);
+    const line = new GeometricLine(x1, x2, y, lineWidth, entityLabel!.color);
+    element.appendChild(line.render());
+  }
+
+  private addText(element: SVGGElement, x1: number, entity: Entity) {
+    const marginTop = 5;
+    const entityLabel = this.entityLabels.getById(entity.label);
+    const y = this.calculateLineY(entity) + fontHeight / 2 + marginTop;
+    const circle = new GeometricCircle(x1, y, entityLabel!.circle);
+    const text = new GeometricText(x1, y, entityLabel!);
+    element.appendChild(circle.render());
+    element.appendChild(text.render());
   }
 
   private calculateLineY(entity: Entity): number {
