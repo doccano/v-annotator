6b52793923ec2b22f39daca05d9e79f97b6a9ab9
Hironsan <light.tree.1.13@gmail.com>
2021-07-15 14:58:05 +0900

    Add shape.spec.ts

---
+++ tests/unit/domain/models/Line/shape.spec.ts
@@ -0,0 +1,25 @@
+import { Circle } from "@/domain/models/Line/Shape";
+import { Font } from "../../../../../src/domain/models/Line/Font";
+import { LabelText } from "../../../../../src/domain/models/Line/Shape";
+
+describe("Circle", () => {
+  it("calculate width", () => {
+    const radius = 3;
+    const circle = new Circle(radius);
+    const expected = radius * 2;
+    expect(circle.width).toEqual(expected);
+  });
+});
+
+describe("LabelText", () => {
+  it("calculate width", () => {
+    const text = "abc";
+    const width = new Map();
+    for (const ch of Array.from(text)) {
+      width.set(ch, 1);
+    }
+    const font = new Font(0, "", "", 0, width);
+    const labelText = new LabelText(text, font);
+    expect(labelText.width).toEqual(text.length);
+  });
+});
