cdc0c6fa6d0fe3e13b3da27841ac6c681fb85941
Hironsan <light.tree.1.13@gmail.com>
2021-08-10 22:15:07 +0900

    Add GeometricLabelText.vue

---
+++ src/components/GeometricLabelText.vue
@@ -0,0 +1,48 @@
+<template>
+  <g>
+    <circle :r="r" :fill="color" :cx="cx" :cy="y" />
+    <text
+      :x="textX"
+      :y="y"
+      fill="grey"
+      dy="0.35em"
+      style="cursor: pointer; userselect: none"
+    >
+      {{ entityLabel.text }}
+    </text>
+  </g>
+</template>
+
+<script lang="ts">
+import { EntityLabel } from "@/domain/models/Line/Shape";
+import Vue, { PropType } from "vue";
+
+export default Vue.extend({
+  props: {
+    x: {
+      type: Number,
+    },
+    y: {
+      type: Number,
+    },
+    entityLabel: {
+      type: Object as PropType<EntityLabel>,
+    },
+  },
+
+  computed: {
+    color(): string {
+      return this.entityLabel!.circle.color;
+    },
+    cx(): number {
+      return this.x + this.r;
+    },
+    r(): number {
+      return this.entityLabel!.circle.radius;
+    },
+    textX(): number {
+      return this.x + this.entityLabel.marginLeft;
+    },
+  },
+});
+</script>
