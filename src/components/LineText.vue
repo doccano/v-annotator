793a160e64a2ba61adeed1e4e9b33cc07f25c03d
Hironsan <light.tree.1.13@gmail.com>
2021-08-10 22:15:45 +0900

    Add LineText.vue

---
+++ src/components/LineText.vue
@@ -0,0 +1,35 @@
+<template>
+  <tspan x="0">
+    <v-span
+      v-for="(span, index) in spans"
+      :key="index"
+      :dx="span.dx"
+      :text="text.substring(span.startOffset, span.endOffset)"
+    />
+  </tspan>
+</template>
+
+<script lang="ts">
+import { Span } from "@/domain/models/Line/TextLine";
+import Vue, { PropType } from "vue";
+import VSpan from "./VSpan.vue";
+
+export default Vue.extend({
+  components: {
+    VSpan,
+  },
+
+  props: {
+    text: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    spans: {
+      type: [] as PropType<Span[]>,
+      default: [],
+      required: true,
+    },
+  },
+});
+</script>
