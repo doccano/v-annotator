72ceb779b5a4fc3bfa05f54e9eb84fdcb3c82713
Hironsan <light.tree.1.13@gmail.com>
2021-08-10 22:14:33 +0900

    Add VSpan.vue

---
+++ src/components/VSpan.vue
@@ -0,0 +1,22 @@
+<template>
+  <tspan :dx="dx">{{ text }}</tspan>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    dx: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+});
+</script>
