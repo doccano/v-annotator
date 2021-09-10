1e9541672dd5e2d6967a340f43e2aaa6ea977c64
Hironsan <light.tree.1.13@gmail.com>
2021-09-10 16:59:04 +0900

    Resolve no exported member error

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,5 +3,23 @@ declare module "*.vue" {
   export default Vue;
 }
 declare module "lodash";
-declare module "vue-virtual-scroller";
 declare module "unicode-bidirectional";
+declare module "vue-virtual-scroller" {
+  import Vue, { ComponentOptions, PluginObject, Component } from "vue";
+  interface PluginOptions {
+    installComponents?: boolean;
+    componentsPrefix?: string;
+  }
+
+  const plugin: PluginObject<PluginOptions> & { version: string };
+
+  export const RecycleScroller: Component<any, any, any, any>;
+  export const DynamicScroller: Component<any, any, any, any>;
+  export const DynamicScrollerItem: Component<any, any, any, any>;
+
+  export function IdState(options?: {
+    idProp?: (vm: any) => any;
+  }): ComponentOptions<Vue> | typeof Vue;
+
+  export default plugin;
+}
