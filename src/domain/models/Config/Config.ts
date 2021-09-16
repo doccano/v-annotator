754612ac4d62f422e7d965537336fb6fc5c284ac
Hironsan <light.tree.1.13@gmail.com>
2021-09-16 15:07:11 +0900

    Add Config

---
+++ src/domain/models/Config/Config.ts
@@ -0,0 +1,19 @@
+interface Config {
+  radius: number;
+  lineWidth: number;
+  labelMargin: number;
+  diameter: number;
+}
+
+const defaultConfig: Config = Object.freeze({
+  radius: 3,
+  lineWidth: 5,
+  get labelMargin(): number {
+    return 5 + this.radius;
+  },
+  get diameter(): number {
+    return this.radius * 2;
+  },
+});
+
+export default defaultConfig;
