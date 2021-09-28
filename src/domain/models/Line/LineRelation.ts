ef99f72e2b8f09c69b82bae26369e9d772ca6612
Hironsan <light.tree.1.13@gmail.com>
2021-09-28 10:42:50 +0900

    Fix LineRelation algorithm

---
+++ src/domain/models/Line/LineRelation.ts
@@ -0,0 +1,60 @@
+import { LevelManager } from "../Label/Entity";
+import { LabelList } from "../Label/Label";
+import { Relation } from "../Label/Relation";
+import { GeometricEntity } from "./LineEntity";
+import { TextLine } from './LineText';
+
+export interface LineRelation {
+  x1: number | undefined;
+  x2: number | undefined;
+  label: string;
+  labelWidth: number;
+  level: number;
+  relation: Relation;
+}
+
+export class RelationLine {
+  private levelManager = new LevelManager();
+  constructor(
+    private relations: Relation[],
+    private relationLabels: LabelList,
+    private textLine: TextLine
+  ) {}
+
+  render(entities: GeometricEntity[]): LineRelation[] {
+    const lineRelations: LineRelation[] = [];
+    this.levelManager.clear();
+    const entityMap: Map<number, GeometricEntity> = new Map();
+    entities.forEach((entity) => {
+      entityMap.set(entity.entity.id, entity);
+    });
+    let x1 = undefined;
+    let x2 = undefined;
+    for (const relation of this.relations) {
+      const fromEntity = entityMap.get(relation.fromId);
+      const toEntity = entityMap.get(relation.toId);
+      const label = this.relationLabels.getById(relation.labelId);
+      if (fromEntity) {
+        x1 = fromEntity.ranges.center();
+        if (fromEntity.entity.startOffset < this.textLine.startOffset) continue;
+      }
+      if (toEntity) {
+        x2 = toEntity.ranges.center();
+        if (toEntity.entity.startOffset < this.textLine.startOffset) continue;
+      }
+      if (x1 === undefined && x2 === undefined) continue;
+      // this.levelManager.update();
+      // const level = this.levelManager.fetchLevel(relation)!;
+      const level = 0;
+      lineRelations.push({
+        x1,
+        x2,
+        level,
+        label: label!.text,
+        labelWidth: label!.width,
+        relation,
+      });
+    }
+    return lineRelations;
+  }
+}
